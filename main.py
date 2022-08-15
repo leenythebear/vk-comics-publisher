f12a637f709b45eaf42d7eb9766033ad6c3f1e14
leeny_the_bear <leeny@inbox.ru>
2022-08-15 15:49:46 +0700

    add func to take random comics

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import os
 from urllib.parse import urlparse
 
+import requests
 
 def get_comics_extension(url):
     parsed_link = urlparse(url)
     return os.path.splitext(parsed_link.path)[1]
+
+
+def get_random_comics(num):
+    comics_url = f"https://xkcd.com/{num}/info.0.json"
+    response = requests.get(comics_url)
+    response.raise_for_status()
+    return response.json()
