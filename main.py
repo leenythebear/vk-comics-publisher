aae011f5157c9f2e8636cb541fb2bd19294c6312
leeny_the_bear <leeny@inbox.ru>
2022-08-18 16:49:24 +0700

    add try-finally to remove unnecessary files when an error occurs

--- main.py
+++ main.py
@@ -30,12 +30,14 @@ if __name__ == "__main__":
     comic_filename = get_comics_filename(comic_extension, comic_name)
     save_comic(comic_url, comic_filename)
 
-    url_for_upload = get_url_for_upload(group_id, token)
-    upload_server_response = upload_comic_to_server(
-        comic_filename, url_for_upload
-    )
-    upload_album_response = upload_comic_in_album(
-        upload_server_response, token
-    )
-    publish_comic(upload_album_response, token, group_id, comic_comment)
-    os.remove(comic_filename)
+    try:
+        url_for_upload = get_url_for_upload(group_id, token)
+        upload_server_response = upload_comic_to_server(
+            comic_filename, url_for_upload
+        )
+        upload_album_response = upload_comic_in_album(
+            upload_server_response, token
+        )
+        publish_comic(upload_album_response, token, group_id, comic_comment)
+    finally:
+        os.remove(comic_filename)
